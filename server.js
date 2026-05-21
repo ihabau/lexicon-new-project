6d0fb80b13668db1ae1d87e57b7d85f99a26f614
ihab Al Ubaidi <www.ihabau@gmail.com>
2026-05-21 11:00:43 +0200

    adding node server.

---
+++ server.js
@@ -0,0 +1,18 @@
+﻿console.log("hello node!!")
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// A test route for the home URL
+app.get('/', (req, res) => {
+    res.json({ message: "Hello World from the Node backend!" });
+});
+
+// Start the server and listen for connections
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
